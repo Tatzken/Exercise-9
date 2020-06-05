30cb859e6ddd293790ceaf25c9717fb5d3ca1394
Tatzken <learner-f8da33@extensionschool.ch>
2020-06-05 10:28:00 +0200

    at chapter 15

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
-// This function takes a boolean parameter
-// that says if password was entered correctly
-// (true = yes, false = no)
-function logIn(passwordIsCorrect) {
-    if (passwordIsCorrect != true) {
-        console.log("Your password is not correct, you can't log in");
-    } else {
-        console.log("Welcome back! You are now signed in."); // else statement was missing, thus sign in was laways executed
-    }
-}
-
-function litersToGallons(liters) {
-    var reslut = liters / 3.785;
-    var message = liters.toString() + " liters are " + result.toString() + " gallons.";
-    console.log(message); // console was written with upper case letter
-}
-
-function isGreatherThan10(number) {
-    if (number > 10) {
-        return "The number is greater than 10.";
-    } else if (number == 10) {
-        return "The number is 10.";
-    } else {
-        return "The number is smaller than 10.";
-    }
-}
+var fruits = ["oranges", "apples", "bananas"];
+var message = "The second value contains " + fruits[1];
+console.log(message);
+fruits[1] = "kiwi";
+message = "The second value contains " + fruits[1];
+console.log(message);
